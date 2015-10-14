6b116cfb2599123fa227463a02052cd9cc9a03d6
Salonee Thanawala <salonee@Salonees-MacBook-Air.local>
2015-10-13 18:25:05 -0700

    Lab8 was updated

---
+++ Lab8/MyListDriver.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class MyListDriver
+{
+   public static void main(String[] args)
+   {
+      //object of MyList
+      MyList<Integer> l = new MyList<Integer>();
+ 
+      System.out.println("Choose one of the following options: ");
+      System.out.println("-     add (enter the letter a)");
+      System.out.println("-     find (enter the letter f)");
+ 
+
+}
