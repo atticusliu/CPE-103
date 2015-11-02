59757c39a70d5d7694cfb2e9e40aca1f1acc16c6
Salonee Thanawala <salonee@Salonees-MacBook-Air.local>
2015-11-02 00:04:14 -0800

    DictionaryMaker updated

--- Project3/DictionaryMaker.java
+++ Project3/DictionaryMaker.java
@@ -6,11 +6,23 @@
  * Project 3
  */
 
+import java.lang.System;
 import java.util.*;
 
 public class DictionaryMaker
 {
+    public static void main(String[] args)
+    {
+        //asking user for input file and saving the file in a temp variable inputFile
+        System.out.println("What is the name of the input file?: ");
+        Scanner sc = new Scanner(System.in);
+        String inputFile = sc.next();
+        sc.nextLine();
 
+        //gets name for output file and sets up file for outputting
+        System.out.println("What is the name of the output file?: ");
+
+    }
 
 
 }
