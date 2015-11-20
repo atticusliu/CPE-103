12bdf67e215a2df0e784acd1639d46d91beb2df6
Atticus Liu <atticusacl@gmail.com>
2015-11-20 11:16:20 -0800

    Project5 initial commit

---
+++ Project5/Sorts.java
@@ -0,0 +1,45 @@
+/*
+ * @authors: Salonee Thanawala, Atticus Liu
+ * @usernames: sthanawala@calpoly.edu, aliu44@calpoly.edu
+ * CPE103-03
+ * 20 November 2015
+ * Project 5
+ */
+
+import java.util.*;
+
+public class Sorts {
+
+    public static <T extends Comparable<? super T>> void selectionSort(T[] arr, int size)
+    {
+        int minIndex;
+        for(int i = 0; i < size; i++)
+        {
+            minIndex = i;
+            for(j = i+1; j <= size; j++)
+            {
+
+            }
+        }
+    }
+
+    public static <T extends Comparable<? super T>> void bubbleSort(T[] arr, int size)
+    {
+
+    }
+
+    public static <T extends Comparable<? super T>> void insertionSort(T[] arr, int size)
+    {
+
+    }
+
+    public static <T extends Comparable<? super T>> void mergeSort(T[] arr, int size)
+    {
+
+    }
+
+    public static <T extends Comparable<? super T>> void quickSort(T[] arr, int size)
+    {
+
+    }
+}
